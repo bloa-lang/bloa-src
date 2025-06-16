53a5e3d3c4e7f5100d2a9aa44f14eb0922d1bb66
AzliRynz <135945845+AzliRynz@users.noreply.github.com>
2025-06-16 15:23:04 +0700

    ...

--- vm.h
+++ vm.h
@@ -1,7 +1,18 @@
 #ifndef VM_H
 #define VM_H
 
-#include "compiler.h"
+#include <stdint.h>
+#include "value.h"
+#include "chunk.h"
+
+#define STACK_MAX 256
+
+typedef struct {
+    Chunk* chunk;
+    uint8_t* ip;
+    Value stack[STACK_MAX];
+    Value* stack_top;
+} VM;
 
 typedef enum {
     INTERPRET_OK,
@@ -14,5 +25,8 @@ void free_vm(VM* vm);
 InterpretResult interpret(VM* vm, const char* source);
 void push(VM* vm, Value value);
 Value pop(VM* vm);
+
+bool is_falsey(Value value);
+bool values_equal(Value a, Value b);
 
 #endif
