b6ea672ff2d3610796c238f276de355409660eb9
AzliRynz <135945845+AzliRynz@users.noreply.github.com>
2025-06-18 08:43:42 +0700

    ...

--- vm.h
+++ vm.h
@@ -6,12 +6,18 @@
 #include "chunk.h"
 
 #define STACK_MAX 256
+#define MAX_CONSTANTS 65536
+
+typedef struct Object Object;
 
 typedef struct {
     Chunk* chunk;
     uint8_t* ip;
     Value stack[STACK_MAX];
     Value* stack_top;
+    Object* objects;
+    size_t bytes_allocated;
+    size_t next_gc;
 } VM;
 
 typedef enum {
@@ -19,6 +25,21 @@ typedef enum {
     INTERPRET_COMPILE_ERROR,
     INTERPRET_RUNTIME_ERROR
 } InterpretResult;
+
+#define GROW_CAPACITY(capacity) \
+    ((capacity) < 8 ? 8 : (capacity) * 2)
+
+#define GROW_ARRAY(type, pointer, oldCount, newCount) \
+    (type*)reallocate(pointer, sizeof(type) * (oldCount), \
+        sizeof(type) * (newCount))
+
+#define FREE_ARRAY(type, pointer, oldCount) \
+    reallocate(pointer, sizeof(type) * (oldCount), 0)
+
+#define ALLOCATE(type, count) \
+    (type*)reallocate(NULL, 0, sizeof(type) * (count))
+
+#define FREE(type, pointer) reallocate(pointer, sizeof(type), 0)
 
 void init_vm(VM* vm);
 void free_vm(VM* vm);
@@ -28,5 +49,13 @@ Value pop(VM* vm);
 
 bool is_falsey(Value value);
 bool values_equal(Value a, Value b);
+
+void* reallocate(void* pointer, size_t oldSize, size_t newSize);
+void collect_garbage(VM* vm);
+void mark_object(VM* vm, Object* object);
+void mark_value(VM* vm, Value value);
+
+#define IS_OBJ(value)     ((value).type == VAL_OBJ)
+#define AS_OBJ(value)     ((value).as.obj)
 
 #endif
