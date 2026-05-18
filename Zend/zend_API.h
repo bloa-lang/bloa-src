5c657c50f2ac4685fe9a3029a2aae23594fd6ae2
AzliRynz <135945845+AzliRynz@users.noreply.github.com>
2026-05-19 05:34:52 +0700

    Update ZEND_RAW_FENTRY macro for PHP 8.4 compatibility

--- Zend/zend_API.h
+++ Zend/zend_API.h
@@ -79,7 +79,11 @@ typedef struct _zend_fcall_info_cache {
 
 #define ZEND_FENTRY(zend_name, name, arg_info, flags)	{ #zend_name, name, arg_info, (uint32_t) (sizeof(arg_info)/sizeof(struct _zend_internal_arg_info)-1), flags, NULL, NULL },
 
-#define ZEND_RAW_FENTRY(zend_name, name, arg_info, flags, frameless_function_infos, doc_comment)   { zend_name, name, arg_info, (uint32_t) (sizeof(arg_info)/sizeof(struct _zend_internal_arg_info)-1), flags, frameless_function_infos, doc_comment },
+#if PHP_VERSION_ID >= 80400
+#define ZEND_RAW_FENTRY(zend_name, name, arg_info, flags, ...) { zend_name, name, arg_info, (uint32_t)(sizeof(arg_info)/sizeof(struct _zend_internal_arg_info)-1), flags, __VA_ARGS__ },
+#else
+#define ZEND_RAW_FENTRY(zend_name, name, arg_info, flags) { zend_name, name, arg_info, (uint32_t)(sizeof(arg_info)/sizeof(struct _zend_internal_arg_info)-1), flags },
+#endif
 
 /* Same as ZEND_NAMED_FE */
 #define ZEND_RAW_NAMED_FE(zend_name, name, arg_info) ZEND_RAW_FENTRY(#zend_name, name, arg_info, 0, NULL, NULL)
