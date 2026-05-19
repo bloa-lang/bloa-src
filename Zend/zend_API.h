192046c2cb159ce11fd1aa07bc2ca6d2634858d6
AzliRynz <135945845+AzliRynz@users.noreply.github.com>
2026-05-19 06:35:19 +0000

    Fix ZEND_RAW_FENTRY macro to include NULL for additional parameters

--- Zend/zend_API.h
+++ Zend/zend_API.h
@@ -82,7 +82,7 @@ typedef struct _zend_fcall_info_cache {
 #if PHP_VERSION_ID >= 80400
 #define ZEND_RAW_FENTRY(zend_name, name, arg_info, flags, frameless_function_infos, doc_comment) { zend_name, name, arg_info, (uint32_t)(sizeof(arg_info)/sizeof(struct _zend_internal_arg_info)-1), flags, frameless_function_infos, doc_comment },
 #else
-#define ZEND_RAW_FENTRY(zend_name, name, arg_info, flags, ...) { zend_name, name, arg_info, (uint32_t)(sizeof(arg_info)/sizeof(struct _zend_internal_arg_info)-1), flags },
+#define ZEND_RAW_FENTRY(zend_name, name, arg_info, flags, ...) { zend_name, name, arg_info, (uint32_t)(sizeof(arg_info)/sizeof(struct _zend_internal_arg_info)-1), flags, NULL, NULL },
 #endif
 
 /* Same as ZEND_NAMED_FE */
